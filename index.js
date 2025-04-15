55e0337b0e253a29860b968d13bb6cc92d478c2e
jash21twelve <jash@21twelveinteractive.in>
2025-04-15 12:28:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// server.js
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+app.get('/healthcheck', (req, res) => {
+  res.send('Uptick Hubspot!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server is running at http://localhost:${PORT}/`);
+});
